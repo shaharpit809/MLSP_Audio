0a6e935b903008d1b873aaca8733261764d9e649
shaharpit809 <shaharpit809@gmail.com>
2018-12-08 21:42:20 -0500

    LSTM

--- dec8_classify.py
+++ dec8_classify.py
@@ -20,6 +20,11 @@ from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import classification_report
+from keras.layers import LSTM
+from keras.layers import Bidirectional
+from keras.layers import TimeDistributed
+from keras.models import Sequential
+from keras.layers import Dense, Activation,Dropout, Flatten, Embedding
 
 
 path_data = 'D:/MLSP Project/Data/Data'
@@ -132,8 +137,6 @@ for i in range(len(f)):
 
 X_train,X_test,y_train,y_test = train_test_split(feature_all,one_hot_encode,test_size = 0.3,random_state=20)
 
-from keras.models import Sequential
-from keras.layers import Dense, Activation,Dropout
 
 model = Sequential()
 
@@ -165,6 +168,16 @@ with open('mlp_model_relu_adadelta.json','w') as j:
     j.write(mlp_model)
 model.save_weights("mlp_relu_adadelta_model.h5")
 
+y_pred_model1 = model.predict(X_test)
+y2 = np.argmax(y_pred_model1,axis=1)
+y_test2 = np.argmax(y_test , axis = 1)
+
+count = 0
+for i in range(y2.shape[0]):
+    if y2[i] == y_test2[i]:
+        count+=1
+        
+        
 
 # json_file = open('model.json', 'r')
 # loaded_model_json = json_file.read()
@@ -195,7 +208,55 @@ model2.compile(loss = 'categorical_crossentropy',optimizer='adadelta',metrics=['
 
 model2.fit(X_train,y_train,nb_epoch=200,batch_size = 5,verbose=1)
 
-mlp_model2 = model2.to_json()
-with open('mlp_model_tanh_adadelta.json','w') as j:
-    j.write(mlp_model2)
-model2.save_weights("mlp_tanh_adadelta_model.h5")
+model2.evaluate(X_test, y_test)
+
+
+# mlp_model2 = model2.to_json()
+# with open('mlp_model_tanh_adadelta.json','w') as j:
+#     j.write(mlp_model2)
+# model2.save_weights("mlp_tanh_adadelta_model.h5")
+
+
+y_pred_model2 = model2.predict(X_test)
+y22 = np.argmax(y_pred_model2,axis=1)
+y_test22 = np.argmax(y_test , axis = 1)
+
+count = 0
+for i in range(y22.shape[0]):
+    if y22[i] == y_test22[i]:
+        count+=1
+X_train2,X_test2,y_train2,y_test2 = train_test_split(feature_all,one_hot_encode,test_size = 0.3,random_state=20)        
+from xgboost import XGBClassifier
+model3 = XGBClassifier()
+model3.fit(X_train2,y_train2[:,0])
+
+y_pred3 = model3.predict(X_test)
+
+count = 0
+for i in range(y_pred3.shape[0]):
+    if y_pred3[i] == y_test2[i]:
+        count+=1   
+        
+# clf = RandomForestClassifier(n_estimators=60,max_features=8,max_depth=None,min_samples_split=3,bootstrap=True,random_state=35)
+# clf = clf.fit(X_train, y_train)
+# #scores = cross_val_score(clf, X_train, y_train, cv=5)
+# #print(scores.mean())
+# y_pred = clf.predict(X_test)
+# for i in range(np.shape(y_test))
+        
+        
+        
+#LSTM
+xt = deepcopy(X_train)    
+X_train2 = np.reshape(X_train, (X_train.shape[0], 1, X_train.shape[1]))
+y_train2 = np.reshape(y_train, (y_train.shape[0], 1, y_train.shape[1]))      
+#n_timesteps = 194544
+model3 = Sequential()
+model3.add(Embedding(2000, 128, input_length=X_train.shape[1], dropout = 0.2))
+model3.add(LSTM(20, input_shape=np.shape(xt)))
+# model3.add(TimeDistributed(Dense(1)))
+# model.add(Flatten())
+model.add(Dense(8, activation='softmax'))
+model3.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+
+model3.fit(X_train, y_train2, epochs=1, batch_size=1, verbose=1)
